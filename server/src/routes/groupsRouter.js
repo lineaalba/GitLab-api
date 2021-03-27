f17a12829b9d3cc39541fa2be2a9a2c2bec413e6
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-27 21:14:36 +0100

    cors

--- server/src/routes/groupsRouter.js
+++ server/src/routes/groupsRouter.js
@@ -9,6 +9,11 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('../middlewares/isLoggedIn.js')
 const getGroups = require('../controllers/getGroups.js')
+const cors = require('cors')
+
+router.use(cors({origin: process.env.CLIENT_URL,
+    credentials: true
+}))
 
 // Get groups if user is logged in
 router.get('/', isLoggedIn, getGroups)
