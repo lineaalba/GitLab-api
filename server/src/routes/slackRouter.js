4f5723024826881ab6b88d2f5e8df2bd40d86819
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-28 11:48:07 +0200

    debug

--- server/src/routes/slackRouter.js
+++ server/src/routes/slackRouter.js
@@ -8,7 +8,11 @@
  const express = require('express')
  const router = express.Router()
  const postSlackUrl = require('../controllers/postSlackUrl.js')
+ const cors = require('cors')
 
+ router.use(cors({origin: process.env.CLIENT_URL,
+    credentials: true
+}))
  // Post slack url
  router.post('/', postSlackUrl)
 
