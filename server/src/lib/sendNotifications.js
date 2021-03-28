8f2436b7355117e9889bb279621330cdb1233f89
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-28 14:20:07 +0200

    debug

--- server/src/lib/sendNotifications.js
+++ server/src/lib/sendNotifications.js
@@ -22,21 +22,25 @@
 // module.exports = sendNotifications
 
 const fetch = require('node-fetch')
-// import Url from '../models/url'
+import Url from '../models/url'
 
 /**
  *  Sends notifications to slack
  */
 const sendNotifications = async () => { 
     try {
-            await fetch('https://hooks.slack.com/services/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', {
-                method: 'POST',
-                // body: JSON.stringify({"text": 'New issue, { ' + response.object_attributes.title + ' }, in project { ' + response.project.name + ' }.'}),
-                body: JSON.stringify({"text": 'testing'}),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })         
+    const urlHooks = await Url.find({})
+
+    urlHooks.forEach(async (element) => {
+        await fetch(element.url, {
+            method: 'POST',
+            // body: JSON.stringify({"text": 'New issue, { ' + response.object_attributes.title + ' }, in project { ' + response.project.name + ' }.'}),
+            body: JSON.stringify({"text": 'testing, id' + element.id}),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })  
+    })         
 
     } catch (error) {
         console.log(error)
