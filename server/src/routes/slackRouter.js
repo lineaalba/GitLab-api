014cca42f169fa27e6fce60c58993ac2f9321185
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-28 12:05:23 +0200

    debug

--- server/src/routes/slackRouter.js
+++ server/src/routes/slackRouter.js
@@ -8,11 +8,11 @@
  const express = require('express')
  const router = express.Router()
  const postSlackUrl = require('../controllers/postSlackUrl.js')
-//  const cors = require('cors')
+ const cors = require('cors')
 
-//  router.use(cors({origin: process.env.CLIENT_URL,
-//     credentials: true
-// }))
+ router.use(cors({origin: 'https://nifty-yalow-9797ba.netlify.app/dashboard',
+    credentials: true
+}))
  // Post slack url
  router.post('/', postSlackUrl)
 
